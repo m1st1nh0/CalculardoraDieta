81c39e106fc538cb340d902931dd95cdf78150ee
m1st1nh0 <lukaschamposki@gmail.com>
2026-06-02 23:51:13 -0300

    Ajustando paleta de cores e layout dos modais

--- render/renderizarModal.js
+++ render/renderizarModal.js
@@ -1,39 +1,61 @@
 export function renderizarModal(kanban) {
-
-  //Adiciona os modais ao HTML do kanban
+  // Adiciona os modais ao HTML do kanban
   kanban.innerHTML = `
-        <div id="myModal1" class="modal">
+    <div id="myModal1" class="modal">
+      <div class="modal-content1">
+        <span class="close" style="order: 2">&times;</span>
 
-          <div class="modal-content1">
-          <span class="close" style="order: 2">&times;</span>
+        <div id="criar-refeicao" style="order: 1">
+          <div class="titulo-criar-refeicao">
+            <h2>Qual refeição você quer planejar?</h2>
+          </div>
 
-            <div id="criar-refeicao" style="order: 1">
-              <input
-                type="text"
-                id="nomeRefeicao"
-                placeholder="Digite o nome (Ex: Almoço)"
-              />
-              <button type="button" id="submitModel1">Criar refeicão</button>
-            </div>
+          <div class="input-botao-criar-refeicao">
+            <input
+              type="text"
+              id="nomeRefeicao"
+              placeholder="Digite o nome (Ex: Almoço)"
+            />
+            <button type="button" id="submitModel1">
+              Criar 
+            </button>
           </div>
         </div>
-        <div id="myModal2" class="modal">
-          <div class="modal-content2">
-            <span class="close" style="order: 3">&times;</span>
-            <div id="criar-itemRefeicao" style="order: 1">
-              <input type="text" id="ingrediente" placeholder="Ingrediente" />
-              <input
-                type="number"
-                id="quantidade"
-                placeholder="Quantidade em g"
-              />
-              <button type="button" id="btnAdcItem">
-                Adicionar ingrediente
-              </button>
-              <button type="button" id="finishbtn">Finalizar Edição</button>
-            </div>
-            <div id="itemRefeicao" style="order: 2"></div>
+      </div>
+    </div>
+
+    <div id="myModal2" class="modal">
+      <div class="modal-content2">
+        <span class="close" style="order: 3">&times;</span>
+
+        <div id="criar-itemRefeicao" style="order: 1">
+          <div class="titulo-criar-itemRefeicao">
+            <h4>Adicione os Ingredientes da refeição</h4>
+          </div>
+          <div class="input-criar-itemRefeicao">
+            <input
+              type="text"
+              id="ingrediente"
+              placeholder="Ingrediente"
+            />
+            <input
+              type="number"
+              id="quantidade"
+              placeholder="Quantidade em g"
+            />
+          </div>
+          <div class="botao-itemRefeicao">
+            <button type="button" id="btnAdcItem">
+              Adicionar
+            </button>
           </div>
         </div>
-    `;
+
+        <div id="itemRefeicao" style="order: 2"></div>
+        <button type="button" id="finishbtn" style="order: 3">
+          Finalizar Edição
+        </button> 
+      </div>
+    </div>
+  `;
 }
